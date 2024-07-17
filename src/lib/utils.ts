e93beb51d15874074a8ffc3da760a093987edd23
david van der Bent <davidvdbent@gmail.com>
2024-07-17 18:27:04 +0200

    first commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -47,7 +47,7 @@ export const formatDuration = (seconds : number) => {
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_URL)
-    return `https://${process.env.VERCEL_URL}${path}`
+    return `https://mediatree.vercel.app/${path}`
   return `http://localhost:${
     process.env.PORT ?? 3000
   }${path}`
