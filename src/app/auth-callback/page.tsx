d70524af4d060cd6bb4472081976bd6ced122209
david van der Bent <davidvdbent@gmail.com>
2024-07-18 09:29:35 +0200

    auth callback change

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -8,22 +8,25 @@ import { Suspense } from "react"
 const Loading = () => (
     <div className="text-center mt-5 text-3xl pb-4">
         <Loader2 className=" pt-20 mx-auto h-8 w-8 animate-spin" />
+        <h2>Loading...</h2>
     </div>
 );
 
-const AuthPage = () => {
+const AuthPage = async () => {
     const router = useRouter()
 
     const searchParams = useSearchParams()
     const origin = searchParams.get('origin')
     // get acces to origin 
 
-    const { data } = useQuery({
-        queryKey: ['auth-callback'],
-        queryFn: async () => await getAuthstatus(),
-        retry: true,
-        retryDelay: 500,
-    })
+    const data = await getAuthstatus()
+
+    // const { data } = useQuery({
+    //     queryKey: ['auth-callback'],
+    //     queryFn: async () => await getAuthstatus(),
+    //     retry: true,
+    //     retryDelay: 500,
+    // })
 
     if(data?.success){
         router.push('/dashboard')
