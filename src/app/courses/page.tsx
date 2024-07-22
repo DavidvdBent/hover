36854cc587e89b508f61a1dc17a2f0766d2a35e4
david van der Bent <davidvdbent@gmail.com>
2024-07-22 10:28:45 +0200

    last edits

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,5 @@
 import Search from "@/components/Search"
 import MaxWidthWrapper from "@/components/ui/MaxWidthWrapper"
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-import { ArrowRight, Bold, Clock, Euro, GraduationCap, Italic, Underline } from "lucide-react"
 import AllCourses from "@/components/AllCourses"
 import { fetchPages } from "@/lib/data"
 import Pagination from "@/components/Pagination"
@@ -21,24 +19,12 @@ const page = async ({searchParams} : {searchParams? : {
       <div className="flex justify-between mt-8 pb-2 border-b-2">
             <Search />
         <div>
-        {/* <ToggleGroup type="multiple">
-        <ToggleGroupItem value="bold" aria-label="Toggle bold" className="mx-4">
-            <Bold className="h-4 w-4 text-gray-500" />
-        </ToggleGroupItem>
-        <ToggleGroupItem value="italic" aria-label="Toggle italic" className="mx-4">
-            <Italic className="h-4 w-4 text-gray-500" />
-        </ToggleGroupItem>
-        <ToggleGroupItem value="underline" aria-label="Toggle underline" className="mx-4">
-            <Underline className="h-4 w-4 text-gray-500" />
-        </ToggleGroupItem>
-        </ToggleGroup> */}
         </div>
       </div>
       <AllCourses query={query} currentPage={currentPage}/>
       <div className="mb-10 flex justify-end mr-7 ">
         <Pagination totalPages = {totalPages} />
       </div>
-
       </MaxWidthWrapper>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,22 +20,22 @@ export default function Home() {
           <div>
             <Gem className="text-blue-500 text-2xl mx-auto my-3"/>
             <h3 className="text-blue-500 border-b-2 border-gray-400 pb-4 text-xl sm:text-2xl w-[250px] mx-auto">High Quality Courses</h3>
-            <p className=" text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our Courses are given and manufactured by handpicked experts in this field. We thrive to give you the best of the best when it comes to knowledge and expertise</p>
+            <p className=" text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our High Quality Courses are designed by top-tier professionals with extensive experience. We aim to provide unmatched learning experiences, ensuring you gain the best knowledge and skills in the industry.</p>
           </div>
           <div>
             <GraduationCap className="text-blue-500 text-2xl mx-auto my-3"/>
             <h3 className="text-blue-500 text-xl sm:text-2xl border-b-2 border-gray-400 pb-4 w-[250px] mx-auto">Taught by Experts</h3>
-            <p className="text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our Courses are given and manufactured by handpicked experts in this field. We thrive to give you the best of the best when it comes to knowledge and expertise</p>
+            <p className="text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our courses are Taught By Experts who are leaders in their fields. With years of experience, they provide you with comprehensive knowledge and practical skills, ensuring you receive the highest quality education available.</p>
           </div>
           <div>
             <MegaphoneOff className="text-blue-500 text-2xl mx-auto my-3"/>
             <h3 className="text-blue-500 text-xl sm:text-2xl border-b-2 border-gray-400 pb-4 w-[250px] mx-auto">Advertising Free</h3>
-            <p className="text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our Courses are given and manufactured by handpicked experts in this field. We thrive to give you the best of the best when it comes to knowledge and expertise</p>
+            <p className="text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our platform is entirely Advertising Free, focusing solely on delivering great content. Enjoy an uninterrupted learning experience, crafted by experts, ensuring you receive top-notch education without distractions.</p>
           </div>
           <div>
             <DollarSign className="text-blue-500 text-2xl mx-auto my-3"/>
             <h3 className="text-blue-500 text-xl sm:text-2xl border-b-2 border-gray-400 pb-4 w-[250px] mx-auto">Refund</h3>
-            <p className="text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our Courses are given and manufactured by handpicked experts in this field. We thrive to give you the best of the best when it comes to knowledge and expertise</p>
+            <p className="text-gray-600 sm:text-lg mt-4 w-[250px] sm:w-[400px] mx-auto md:mx-0 md:w-auto">Our Refund policy guarantees your satisfaction. If our courses don't meet your expectations, we offer a hassle-free refund process. We are committed to ensuring you receive the best value and learning experience, risk-free.</p>
           </div>
         </div>
         <Image className="mt-28 mb-10 rounded-2xl"height={1160} width={1160} quality={100} src='/max.jpg' alt=""/>
